303d208244bf6e415dd64d7182ed60d611b15630
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2024-11-05 17:12:01 -0600

    Going back to minimal changes

--- bin/device-install.sh
+++ bin/device-install.sh
@@ -6,76 +6,75 @@ set -e
 
 # Usage info
 show_help() {
-    cat <<EOF
+	cat <<EOF
 Usage: $(basename $0) [-h] [-p ESPTOOL_PORT] [-P PYTHON] [-f FILENAME|FILENAME]
 Flash image file to device, but first erasing and writing system information"
 
     -h               Display this help and exit
-    -p ESPTOOL_PORT  Set the environment variable for ESPTOOL_PORT. If not set, ESPTOOL iterates all ports (Dangerous).
-    -P PYTHON        Specify alternate python interpreter to invoke esptool. (Default: "$PYTHON")
-    -f FILENAME      The .bin file to flash. Custom to your device type and region.
+    -p ESPTOOL_PORT  Set the environment variable for ESPTOOL_PORT.  If not set, ESPTOOL iterates all ports (Dangerous).
+    -P PYTHON        Specify alternate python interpreter to use to invoke esptool. (Default: "$PYTHON")
+    -f FILENAME      The .bin file to flash.  Custom to your device type and region.
 
 EOF
 }
 
 while getopts ":hp:P:f:" opt; do
-    case "${opt}" in
-    h)
-        show_help
-        exit 0
-        ;;
-    p)
-        export ESPTOOL_PORT=${OPTARG}
-        ;;
-    P)
-        PYTHON=${OPTARG}
-        ;;
-    f)
-        FILENAME=${OPTARG}
-        ;;
-    *)
-        echo "Invalid flag."
-        show_help >&2
-        exit 1
-        ;;
-    esac
+	case "${opt}" in
+	h)
+		show_help
+		exit 0
+		;;
+	p)
+		export ESPTOOL_PORT=${OPTARG}
+		;;
+	P)
+		PYTHON=${OPTARG}
+		;;
+	f)
+		FILENAME=${OPTARG}
+		;;
+	*)
+		echo "Invalid flag."
+		show_help >&2
+		exit 1
+		;;
+	esac
 done
 shift "$((OPTIND - 1))"
 
-if [ -z "$FILENAME" ] && [ -n "$1" ]; then
-    FILENAME=$1
-    shift
-fi
+[ -z "$FILENAME" -a -n "$1" ] && {
+	FILENAME=$1
+	shift
+}
 
 # Check if esptool is available
 if command -v esptool.py >/dev/null 2>&1; then
-    ESPTOOL_CMD="esptool.py"
-elif "$PYTHON" -m esptool version >/dev/null 2>&1; then
-    ESPTOOL_CMD="$PYTHON -m esptool"
+	ESPTOOL_CMD="esptool.py"
+elif command -v esptool >/dev/null 2>&1; then
+	ESPTOOL_CMD="esptool"
 else
-    echo "esptool not found. Please install esptool via pip or pipx."
-    exit 1
+	ESPTOOL_CMD="$PYTHON -m esptool"
 fi
 
-if [ -f "${FILENAME}" ] && [[ "${FILENAME}" != *"update"* ]]; then
-    echo "Trying to flash ${FILENAME}, but first erasing and writing system information"
-    "$ESPTOOL_CMD" erase_flash
-    "$ESPTOOL_CMD" write_flash 0x00 "${FILENAME}"
-    # Account for S3 board's different OTA partition
-    if [[ "${FILENAME}" != *"s3"* && "${FILENAME}" != *"-v3"* && "${FILENAME}" != *"t-deck"* && "${FILENAME}" != *"wireless-paper"* && "${FILENAME}" != *"wireless-tracker"* && "${FILENAME}" != *"station-g2"* && "${FILENAME}" != *"unphone"* ]]; then
-        if [[ "${FILENAME}" != *"esp32c3"* ]]; then
-            "$ESPTOOL_CMD" write_flash 0x260000 bleota.bin
-        else
-            "$ESPTOOL_CMD" write_flash 0x260000 bleota-c3.bin
-        fi
-    else
-        "$ESPTOOL_CMD" write_flash 0x260000 bleota-s3.bin
-    fi
-    "$ESPTOOL_CMD" write_flash 0x300000 littlefs-*.bin
+if [ -f "${FILENAME}" ] && [ -n "${FILENAME##*"update"*}" ]; then
+	echo "Trying to flash ${FILENAME}, but first erasing and writing system information"
+	$ESPTOOL_CMD erase_flash
+	$ESPTOOL_CMD write_flash 0x00 ${FILENAME}
+	# Account for S3 board's different OTA partition
+	if [ -n "${FILENAME##*"s3"*}" ] && [ -n "${FILENAME##*"-v3"*}" ] && [ -n "${FILENAME##*"t-deck"*}" ] && [ -n "${FILENAME##*"wireless-paper"*}" ] && [ -n "${FILENAME##*"wireless-tracker"*}" ] && [ -n "${FILENAME##*"station-g2"*}" ] && [ -n "${FILENAME##*"unphone"*}" ]; then
+		if [ -n "${FILENAME##*"esp32c3"*}" ]; then
+			$ESPTOOL_CMD write_flash 0x260000 bleota.bin
+		else
+			$ESPTOOL_CMD write_flash 0x260000 bleota-c3.bin
+		fi
+	else
+		$ESPTOOL_CMD write_flash 0x260000 bleota-s3.bin
+	fi
+	$ESPTOOL_CMD write_flash 0x300000 littlefs-*.bin
 
 else
-    show_help
-    echo "Invalid file: ${FILENAME}"
+	show_help
+	echo "Invalid file: ${FILENAME}"
 fi
 
 exit 0
--- bin/device-update.bat
+++ bin/device-update.bat
@@ -3,40 +3,35 @@
 set PYTHON=python
 
 REM Check if esptool is available
-WHERE esptool >nul 2>&1
+WHERE esptool.exe >nul 2>&1
 IF %ERRORLEVEL% EQU 0 (
     SET "ESPTOOL_CMD=esptool"
 ) ELSE (
-    ECHO esptool not found. Please install esptool via pip or pipx.
-    GOTO EOF
+    SET "ESPTOOL_CMD=%PYTHON% -m esptool"
 )
 
 goto GETOPTS
-
 :HELP
 echo Usage: %~nx0 [-h] [-p ESPTOOL_PORT] [-P PYTHON] [-f FILENAME^|FILENAME]
 echo Flash image file to device, leave existing system intact.
 echo.
 echo     -h               Display this help and exit
-echo     -p ESPTOOL_PORT  Set the environment variable for ESPTOOL_PORT. If not set, ESPTOOL iterates all ports (Dangerous).
-echo     -P PYTHON        Specify alternate python interpreter to invoke esptool. (Default: %PYTHON%)
-echo     -f FILENAME      The *update.bin file to flash. Custom to your device type.
+echo     -p ESPTOOL_PORT  Set the environment variable for ESPTOOL_PORT.  If not set, ESPTOOL iterates all ports (Dangerous).
+echo     -P PYTHON        Specify alternate python interpreter to use to invoke esptool. (Default: %PYTHON%)
+echo     -f FILENAME      The *update.bin file to flash.  Custom to your device type.
 goto EOF
 
 :GETOPTS
 if /I "%1"=="-h" goto HELP
 if /I "%1"=="--help" goto HELP
-if /I "%1"=="-F" set "FILENAME=%2" & SHIFT & SHIFT & goto GETOPTS
-if /I "%1"=="-f" set "FILENAME=%2" & SHIFT & SHIFT & goto GETOPTS
-if /I "%1"=="-p" set ESPTOOL_PORT=%2 & SHIFT & SHIFT & goto GETOPTS
-if /I "%1"=="-P" set "PYTHON=%2" & SHIFT & SHIFT & goto GETOPTS
-if NOT "%1"=="" (
-    set "FILENAME=%1"
-    SHIFT
-    goto GETOPTS
-)
+if /I "%1"=="-F" set "FILENAME=%2" & SHIFT
+if /I "%1"=="-f" set "FILENAME=%2" & SHIFT
+if /I "%1"=="-p" set ESPTOOL_PORT=%2 & SHIFT
+if /I "%1"=="-P" set PYTHON=%2 & SHIFT
+SHIFT
+IF NOT "%1"=="" goto GETOPTS
 
-IF "__%FILENAME%__" == "____" (
+IF "%FILENAME%"=="" (
     echo "Missing FILENAME"
     goto HELP
 )
